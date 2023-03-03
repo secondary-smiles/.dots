bedef239b05da527b4b7dcf057cd11768c24508d
secondary-smiles <shavkinder@gmail.com>
2023-03-03 01:45:24 -0500

    Add .zshrc

---
+++ zsh/install-zsh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Utils
+source ./logger.sh
+logfile install-zsh.log
+
+# Install deps
+log "installing deps.."
+yay -S zsh curl starship
+check
+
+# install oh-my-zsh
+log "setting up oh-my-zsh.."
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+check
+
+# change default shell
+log "setting $(which zsh) as the default shell.."
+sudo chsh -s /usr/bin/zsh $(whoami)
+check
+
+# link .zshrc file
+log "symlinking .zshrc.."
+ln -sf ~/.dots/zsh/.zshrc ~/.zshrc
+check
+
+# install zsh plugins
+log "installing zsh-syntax-highlighting.."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+check
+
+log "installing zsh-autosuggestions.."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+check
+
+log "setup zsh dotfiles successfully!"
