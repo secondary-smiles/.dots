a9cc7f0ca15e003a7c73e534b320f73a74e7b403
secondary-smiles <shavkinder@gmail.com>
2023-03-03 01:44:03 -0500

    Initial commit

---
+++ gen-install-scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# iterate over all subdirs
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd "{}" && touch install-$(basename "$PWD").sh' \;
+
+# add execute perms
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd "{}" && chmod +x install-$(basename "$PWD").sh' \;
+
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd "{}" && cp ../logger.sh .' \;
