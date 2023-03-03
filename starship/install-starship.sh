ef06bc6e002ad98762d765dd5092b4412f7369df
secondary-smiles <shavkinder@gmail.com>
2023-03-03 13:53:40 -0500

    Restore config files
    - Accidentally messed them up with prev commit

--- starship/install-starship.sh
+++ starship/install-starship.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+cd ~/.dots/starship
+
+# Utils
+source ~/.dots/util.sh
+logfile install-starship.log
+
+# Install deps
+deps starship
+
+# Link starship.toml
+link ~/.dots/starship/starship.toml ~/.config/starship.toml
