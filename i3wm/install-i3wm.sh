58fc2582ee295f2f0bfd9d683a68930b7de4fd76
secondary-smiles <shavkinder@gmail.com>
2023-03-04 01:32:13 -0500

    Add i3wm config
    - Why have I redone this one like 20 times

--- i3wm/install-i3wm.sh
+++ i3wm/install-i3wm.sh
@@ -7,5 +7,17 @@ source ~/.dots/util.sh
 logfile install-i3wm.log
 
 # Install deps
-# deps
+deps i3-wm kitty rofi bumblebee-status pulseaudio playerctl picom nitrogen
 
+# Link config file
+link ~/.dots/i3wm/config ~/.config/i3/config
+
+# Enable pulseaudio daemon
+log "enabling pulseaudio"
+systemctl --user enable pulseaudio
+systemctl --user start pulseaudio
+
+# Apply changes
+log "restarting i3wm"
+i3-msg restart
+check
